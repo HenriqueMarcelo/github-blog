3d25cb97211fc7936599e93ee7a1c9bcf028d5a6
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-12-13 09:17:16 -0300

    First libs

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: '@rocketseat/eslint-config/react',
+  rules: {
+    'no-restricted-globals': 1,
+    'no-unused-vars': 1,
+    'prefer-template': 1,
+    'no-empty': 1,
+    'jsx-a11y/alt-text': 0,
+  },
+}
